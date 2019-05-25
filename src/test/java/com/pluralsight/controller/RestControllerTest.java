de023836f2b164f1c02ec5261d4de8d4ce5ae985
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2019-05-25 18:36:01 +0200

    Hasta la unidad 5

--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -12,8 +12,24 @@ import com.pluralsight.model.Ride;
 import org.junit.Test;
 
 public class RestControllerTest {
+	
+	@Test(timeout=4000)
+	public void testCreateRide() {
+		RestTemplate restTemplate = new RestTemplate();
+		
+		Ride ride = new Ride();
+		ride.setName("Homemade Trail Vacation");
+		ride.setDuration(31);
+		
+		ride = restTemplate.postForObject("http://localhost:8080/ride_tracker/ride", 
+				ride, 
+				Ride.class);
+		
+		System.out.println("Ride" + ride);
+	}
+	
 
-	@Test(timeout=3000)
+	@Test(timeout=4000)
 	public void testGetRides() {
 		RestTemplate restTemplate = new RestTemplate();
 
